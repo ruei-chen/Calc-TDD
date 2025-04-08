b7a72dceaabb1312945f3e23cfe09b91c22bea2c
ruei-chen <wang20020801@gmail.com>
2025-04-08 22:32:05 +0800

    Test CI with coverage and flake8

--- Calc.py
+++ Calc.py
@@ -1,11 +1,15 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+    
     def sub(self, a, b):
         return a - b
+    
     def mul(self, a, b):
         return a * b
+    
     def div(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero")
         return a / b
+    
--- CalcTest.py
+++ CalcTest.py
@@ -1,26 +1,33 @@
 import unittest
 from Calc import Calculator  # The class we are going to implement
 
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+        
     def test_sub(self):
         calc = Calculator()
         result = calc.sub(5, 3)
         self.assertEqual(result, 2)
+        
     def test_mul(self):
         calc = Calculator()
         result = calc.mul(5, 3)
         self.assertEqual(result, 15)
+        
     def test_div(self):
         calc = Calculator()
         self.assertEqual(calc.div(10, 2), 5.0)
+        
     def test_div_by_zero(self):
         calc = Calculator()
         with self.assertRaises(ValueError):
             calc.div(10, 0)
 
+
 if __name__ == "__main__":
     unittest.main()
+    
